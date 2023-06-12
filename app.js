a3ffbfa9ee0643101609c43f8aacc768885a5181
Shaikh-Umar-Farooq <ushaikh_be21@thapar.edu>
2023-06-13 02:46:24 +0530

    hhh

--- app.js
+++ app.js
@@ -253,6 +253,4 @@ connectDB().then(() => {
         console.log('Server started on port 3000');
       })
 })
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server started on port 3000');
-});
+
